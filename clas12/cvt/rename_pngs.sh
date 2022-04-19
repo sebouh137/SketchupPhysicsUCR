a318f3405064f91e2f56f00eecd888ca51f2ae29
Sebouh Paul <sebouh.paul@gmail.com>
2022-04-19 15:26:11 -0400

    copied over skp files from other projects into one repository

---
+++ clas12/cvt/rename_pngs.sh
@@ -0,0 +1,38 @@
+mv cvt0001.png cvt.png
+mv cvt0002.png svt_module.png
+mv cvt0003.png bmtc_tx.png
+mv cvt0004.png bmtc_ty.png
+mv cvt0005.png bmtc_tz.png
+mv cvt0006.png bmtc_rx.png
+mv cvt0007.png bmtc_ry.png
+mv cvt0008.png bmtc_rz.png
+mv cvt0009.png bmtc_d0.png
+mv cvt0010.png bmtc_phi.png
+mv cvt0011.png bmtc_z0.png
+mv cvt0012.png bmtc_tandip.png
+
+mv cvt0013.png bmtz_tx.png
+mv cvt0014.png bmtz_ty.png
+mv cvt0015.png bmtz_tz.png
+mv cvt0016.png bmtz_rx.png
+mv cvt0017.png bmtz_ry.png
+mv cvt0018.png bmtz_rz.png
+mv cvt0019.png bmtz_d0.png
+mv cvt0020.png bmtz_phi.png
+mv cvt0021.png bmtz_z0.png
+mv cvt0022.png bmtz_tandip.png
+
+mv cvt0023.png svt_tx.png
+mv cvt0024.png svt_ty.png
+mv cvt0025.png svt_tz.png
+mv cvt0026.png svt_rx.png
+mv cvt0027.png svt_ry.png
+mv cvt0028.png svt_rz.png
+mv cvt0029.png svt_d0.png
+mv cvt0030.png svt_phi.png
+mv cvt0031.png svt_z0.png
+mv cvt0032.png svt_tandip.png
+
+mv cvt0033.png bmtc_snl.png
+mv cvt0034.png bmtz_snl.png
+mv cvt0035.png svt_snl.png
