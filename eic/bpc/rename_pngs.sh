3bcae22a48c4c3b3cfdfc8326704d21a9dde3021
Sebouh Paul <sebouh.paul@gmail.com>
2022-05-18 16:21:10 -0400

    updated BPC model

--- eic/bpc/rename_pngs.sh
+++ eic/bpc/rename_pngs.sh
@@ -3,20 +3,20 @@ cp CBlocks_with_EJ200s0001.png $outdir/CBlock.png
 cp CBlocks_with_EJ200s0002.png $outdir/BPC.png
 cp CBlocks_with_EJ200s0003.png $outdir/layer.png
 cp CBlocks_with_EJ200s0004.png $outdir/spiderweb_scint.png
-cp BPC0001.png $outdir/endcap.png
-cp BPC0002.png $outdir/BPC_closed.png
-cp BPC0003.png $outdir/BPC_open.png
-cp BPC0004.png $outdir/BPC_open_full_beampipe.png
-cp BPC0005.png $outdir/layer_structure.png
-cp BPC0006.png $outdir/adjacent.png
-cp BPC0007.png $outdir/adjacent_2.png
-cp BPC0008.png $outdir/explode_view.png
-cp BPC0009.png $outdir/absorber.png
-cp BPC0010.png $outdir/scint_cover.png
-cp BPC0011.png $outdir/scint_from_upstream.png
-cp BPC0012.png $outdir/scint_from_downstream.png
-cp BPC0013.png $outdir/pcb.png
 
+cp BPC0001.png $outdir/BPC_closed.png
+cp BPC0002.png $outdir/BPC_open.png
+#cp BPC0004.png $outdir/BPC_open_full_beampipe.png
+cp BPC0003.png $outdir/layer_structure.png
+cp BPC0004.png $outdir/explode_view.png
+cp BPC0005.png $outdir/absorber.png
+cp BPC0006.png $outdir/scint_cover.png
+cp BPC0007.png $outdir/scint_from_upstream.png
+cp BPC0008.png $outdir/scint_from_downstream.png
+cp BPC0009.png $outdir/pcb.png
+cp BPC0010.png $outdir/endcap.png
+cp BPC0011.png $outdir/adjacent.png
+cp BPC0012.png $outdir/adjacent_2.png
 
 cd $outdir
 git pull
